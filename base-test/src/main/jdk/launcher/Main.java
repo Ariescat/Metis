3c8f9ee47eafe6ac03169c92202df05778f5931c
毒豆芽 <1275563227@qq.com>
2019-08-23 15:46:38 +0800

    launcher

--- base-test/src/main/jdk/launcher/Main.java
+++ base-test/src/main/jdk/launcher/Main.java
@@ -12,25 +12,22 @@ public class Main {
     public static void main(String[] args) throws Exception {
         String bootClassPath = System.getProperty("sun.boot.class.path");
         String property = System.getProperty("java.security.manager");
-        String dirs = System.getProperty("java.ext.dirs");
-        String path = System.getProperty("java.class.path");
+        String dirs = System.getProperty("java.ext.dirs"); // ExtClassLoader
+        String path = System.getProperty("java.class.path"); // AppClassLoader
 
         System.err.println(bootClassPath);
         System.err.println(property);
-        System.err.println(path);
         System.err.println(dirs);
+        System.err.println(path);
 
         Method getClassPath = Launcher.class.getDeclaredMethod("getClassPath", String.class);
         getClassPath.setAccessible(true);
         File[] files = (File[]) getClassPath.invoke(null, path);
 
-//        for (File file : files) {
-//            System.err.println(file);
-//        }
-
 //        Method pathToURLs = Launcher.class.getDeclaredMethod("pathToURLs", File[].class);
 //        pathToURLs.setAccessible(true);
 //        pathToURLs.invoke(Launcher.class, files);
+
 
 
         Field props = System.class.getDeclaredField("props");
@@ -38,9 +35,9 @@ public class Main {
         Properties properties = (Properties) props.get(null);
         System.err.println(properties.stringPropertyNames());
 
-
         ClassLoader cl = Main.class.getClassLoader();
         System.out.println("ClassLoader is:" + cl.toString());
+        // AppClassLoader的parent是ExtClassLoader，ExtClassLoader的parent是null
         System.out.println("ClassLoader\'s parent is:" + cl.getParent().toString());
         System.out.println("ClassLoader\'s grand father is:" + cl.getParent().getParent().toString());
 
