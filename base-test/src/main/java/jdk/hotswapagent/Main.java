9739dc53cadff69dc699909c6de743115074eed1
Ariescat <1275563227@qq.com>
2020-01-08 17:44:27 +0800

    feat(hotswapagent): 研究 GitHub上的 hotswapagent 项目

---
+++ base-test/src/main/java/jdk/hotswapagent/Main.java
@@ -0,0 +1,27 @@
+package jdk.hotswapagent;
+
+import jdk.hotswap.Person;
+
+/**
+ * -XXaltjvm=dcevm -javaagent:C:\Users\毒豆芽\Desktop\hotswap-agent-1.3.0.jar
+ *
+ * @author Ariescat
+ * @version 2020/1/7 20:00
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Person p = new Person();  //内存只有一个实例对象
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(2000);
+                    p.sayHello();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        ).start();
+
+    }
+}
---
+++ base-test/src/main/java/jdk/hotswapagent/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 研究 GitHub上的 hotswapagent 项目
+ * {@see https://github.com/HotswapProjects/HotswapAgent }
+ *
+ * @author Ariescat
+ * @version 2020/1/7 19:59
+ */
+package jdk.hotswapagent;
