6637b5594ab85f858c016e2d7a95c14a2ba9d95f
ariescat <1275563227@qq.com>
2021-05-17 15:22:54 +0800

    feat(leetcode): 993. 二叉树的堂兄弟节点

--- metis-algorithm/src/main/java/com/ariescat/metis/leetcode/utils/TreeNode.java
+++ metis-algorithm/src/main/java/com/ariescat/metis/leetcode/utils/TreeNode.java
@@ -17,4 +17,28 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    public static TreeNode build(Integer[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        TreeNode[] nodes = new TreeNode[arr.length];
+        nodes[0] = new TreeNode(arr[0]);
+        if (arr.length > 1) {
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] == null) continue;
+                // 父索引
+                int pIndex = (i - 1) / 2;
+                TreeNode node = nodes[i] = new TreeNode(arr[i]);
+                if (i % 2 == 1) {
+                    // left
+                    nodes[pIndex].left = node;
+                } else {
+                    // right
+                    nodes[pIndex].right = node;
+                }
+            }
+        }
+        return nodes[0];
+    }
 }
